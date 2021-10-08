690425024c1a76a3155a393c5af20454ff8f281b
Sébas Ferket <s131250@ap.be>
2021-10-08 10:27:58 +0200

    print("Misschien vind je hier al de eerste fout?") a ="10" b ="20" print("a"*5) print(("a"+"b")/3) hexster = hex(25) Print("Als je dit ziet in de output ben je op het einde van de opdracht geraakt. Proficiat!")fening fouten maken

---
+++ labo03/hello_world.py
@@ -0,0 +1,9 @@
+greeting = 'hello' #variable for storing the greeting
+planet = 'world' #variable for storing the planet
+greeting + ' ' + planet #combine it all and print it 
+
+print("hello")
+print(planet)
+print(12*18)
+result = 112 / 14
+print('The result is', result)
